c31c8246e84ebbb4ab660ee0be9c79a431f4a5ff
EDT777 <1445417290@qq.com>
2020-04-11 01:57:31 +0800

    两数相加

--- JAVA算法训练合集/src/两数之和/text.java
+++ JAVA算法训练合集/src/两数之和/text.java
@@ -1,5 +1,27 @@
-package 两数之和;
+package 涓ゆ暟涔嬪拰;
+
+import java.util.Arrays;
 
 public class text {
+		
+	 public static int[] twoSum(int[] nums, int target) {
+		 
+		  for (int i = 0; i < nums.length; i++) {
+	            for (int j = i + 1; j < nums.length; j++) {
+	                if ( target == nums[j]+ nums[i]) {
+	                    return new int[] { i, j };
+	                }
+	            }
+	        }
+	        throw new IllegalArgumentException("No two sum solution");
+	    }
 
+
+	    
+	 
+	 public static void main(String[] args) {
+		int[] a1 = {1,7,2,15};
+		int target =3;
+		System.out.println(Arrays.toString(twoSum(a1,target)));
+	}
 }
